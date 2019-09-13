6a1f4b6aae57b65745945551d9e6d95d4f118a8e
wangfeng <wangfengbabe@163.com>
2019-09-13 14:38:45 +0800

    feat: 增加数据库访问orm，mybatis
    mapper 注解形式访问
    使用mybatis-generator生成mapper 和sqlmap文件，修改 自动生成主键及文件名

---
+++ sql/demo.sql
@@ -0,0 +1,31 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80016
+ Source Host           : localhost:3306
+ Source Schema         : iadmin
+
+ Target Server Type    : MySQL
+ Target Server Version : 80016
+ File Encoding         : 65001
+
+ Date: 13/09/2019 14:36:11
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for demo
+-- ----------------------------
+DROP TABLE IF EXISTS `demo`;
+CREATE TABLE `demo` (
+  `id` bigint(64) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+SET FOREIGN_KEY_CHECKS = 1;
--- src/main/java/com/github/wangfeng/iadmin/IAdminApplication.java
+++ src/main/java/com/github/wangfeng/iadmin/IAdminApplication.java
@@ -1,10 +1,12 @@
 package com.github.wangfeng.iadmin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
+@MapperScan(basePackages = {"com.github.wangfeng.iadmin.dao"})
 public class IAdminApplication {
 
     public static void main(String[] args) {
