0644836dc16848761ae7aa99e8f1dde09c26734e
wangfeng <wangfengbabe@163.com>
2019-09-13 13:46:06 +0800

    feat: 项目初始化 + thymeleaf 视图解析
    1. thymeleaf 作为视图解析模板
    2. 增加 thymeleaf sample

---
+++ src/main/java/com/github/wangfeng/iadmin/web/demo/DemoController.java
@@ -0,0 +1,18 @@
+package com.github.wangfeng.iadmin.web.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/demo")
+@Slf4j
+public class DemoController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String demoIndex() {
+        return "views/demo/index";
+    }
+
+}
