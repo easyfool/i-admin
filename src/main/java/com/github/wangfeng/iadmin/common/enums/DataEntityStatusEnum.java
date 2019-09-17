a7be2aad5b78823046f7ea6c63236ebcb2059400
wangfeng <wangfeng@unionpayintl.com>
2019-09-17 14:04:22 +0800

    fix: 增加数据状态

--- src/main/java/com/github/wangfeng/iadmin/common/enums/DataEntityStatusEnum.java
+++ src/main/java/com/github/wangfeng/iadmin/common/enums/DataEntityStatusEnum.java
@@ -1,5 +1,6 @@
 package com.github.wangfeng.iadmin.common.enums;
 
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,9 +11,15 @@ import lombok.Getter;
 @Getter
 public enum DataEntityStatusEnum {
     STATTUS_NORMAL(0, "正常"),
-    STATTUS_BANNED(1, "禁用"),
-    STATTUS_LOCKED(2, "锁定"),
-    STATTUS_DELTED(3, "删除"),
+    STATTUS_DELTED(1, "删除"),
+    STATUS_DISABLE(2, "不可用"),
+    STATUS_FREEZE(3, "冻结"),
+    STATUS_AUDIT(4, "授权"),
+    STATUS_AUDIT_BACK(5, ""),
+    STATTUS_BANNED(6, "禁用"),
+    //STATTUS_LOCKED(7, "锁定"),
+    //STATTUS_LOCKED(8, "锁定"),
+    STATUS_DRAFT(9, "锁定")
     ;
     private int statusCode;
     private String descrition;
